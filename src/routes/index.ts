6fdfa54e43206c081c7a363e5078952703fd31c6
Lucas Vilar <lucasdantasvilar@hotmail.com>
2022-07-23 17:52:15 -0300

    feat: add login route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import investimentoRouter from './investimentos';
+import loginRouter from './login';
 
 const router = Router();
 
+router.use('/login', loginRouter);
 router.use('/investimentos', investimentoRouter);
 
 export default router;
