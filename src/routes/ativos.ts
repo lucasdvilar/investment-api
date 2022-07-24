09bae58cc9af127140f21507b67c79cdb000e322
Lucas Vilar <lucasdantasvilar@hotmail.com>
2022-07-24 18:08:04 -0300

    feat: add cliente/:id route

--- src/routes/ativos.ts
+++ src/routes/ativos.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import AssetController from "../controllers/Asset";
+import auth from "../middlewares/auth";
+import validateId from "../middlewares/validateId";
 
 const router = Router();
 
+router.get('/cliente/:id', auth, validateId)
 router.get('/:id', new AssetController().getById);
 
 export default router;
