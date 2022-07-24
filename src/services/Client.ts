7aae52ca554476f19e093acf605501ec546cce5e
Lucas Vilar <lucasdantasvilar@hotmail.com>
2022-07-24 15:23:27 -0300

    feat: add deposit method into ClientService

--- src/services/Client.ts
+++ src/services/Client.ts
@@ -1,6 +1,7 @@
 import HttpException from "../helpers/HttpException";
 import Jwt from "../helpers/Jwt";
 import { IClient } from "../interfaces/IClient";
+import IDeposit from '../interfaces/IDeposit';
 import ClientModel from "../models/Client";
 import connection from "../models/connection";
 
@@ -15,6 +16,13 @@ class ClientService {
     const clientInfo = await this.model.getByUsernameAndPw(client);
     if (!clientInfo) throw new HttpException(401, 'Invalid username or password');
     return new Jwt().generateToken(clientInfo);
+  }
+
+  public deposit = async (clientId: number, amount: number): Promise<IDeposit> => {
+    const client = await this.model.getById(clientId);
+    const newBalance = client.balance + amount;
+    await this.model.update(clientId, newBalance);
+    return { codCliente: clientId, valor: amount };
   }
 }
 
