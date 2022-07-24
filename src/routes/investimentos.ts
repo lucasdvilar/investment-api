18330755be6f039d9db92522d0f633e91ee4f9d1
Lucas Vilar <lucasdantasvilar@hotmail.com>
2022-07-24 02:19:13 -0300

    feat: add investimentos/vender route

--- src/routes/investimentos.ts
+++ src/routes/investimentos.ts
@@ -4,5 +4,6 @@ import ClientAssetController from "../controllers/ClientAsset";
 const router = Router();
 
 router.post('/comprar', new ClientAssetController().purchase);
+router.post('vender');
 
 export default router;
