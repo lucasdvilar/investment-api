1f31fc52e2abd284a788c68ae45d8ad78d9e2c7e
Lucas Vilar <lucasdantasvilar@hotmail.com>
2022-07-24 16:38:31 -0300

    feat: add withdrawal validation

--- src/routes/conta.ts
+++ src/routes/conta.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import ClientController from "../controllers/Client";
 import auth from "../middlewares/auth";
 import validateDeposit from "../middlewares/validateDeposit";
+import validateWithdrawal from "../middlewares/validateWithdrawal";
 
 const router = Router();
 
 router.post('/deposito', validateDeposit, new ClientController().deposit);
-router.post('/saque', auth, new ClientController().withdrawal);
+router.post('/saque', auth, validateWithdrawal, new ClientController().withdrawal);
 router.get('/:cod-cliente', auth);
 
 export default router;
