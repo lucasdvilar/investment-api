1cb2368b84791cb8746b22d95a028b9b5ae6b45e
Lucas Vilar <lucasdantasvilar@hotmail.com>
2022-07-24 15:41:10 -0300

    fix: add greater validation in validateDeposit

--- src/middlewares/validateDeposit.ts
+++ src/middlewares/validateDeposit.ts
@@ -6,7 +6,7 @@ const validateDeposit = (req: Request, _res: Response, next: NextFunction) => {
   const deposit = req.body;
   const { error } = Joi.object({
     clientId: Joi.number().required(),
-    amount: Joi.number().required(),
+    amount: Joi.number().greater(0).required(),
   }).validate(deposit);
   if (error) {
     throw new HttpException(400, error.details[0].message);
