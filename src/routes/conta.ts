fe42beaba1d9fadc0b3d248f10ef486f8f7c8020
Lucas Vilar <lucasdantasvilar@hotmail.com>
2022-07-24 15:13:03 -0300

    feat: add the other conta routes

--- src/routes/conta.ts
+++ src/routes/conta.ts
@@ -3,6 +3,8 @@ import auth from "../middlewares/auth";
 
 const router = Router();
 
+router.post('/deposito');
+router.post('/saque', auth);
 router.get('/:cod-cliente', auth);
 
 export default router;
